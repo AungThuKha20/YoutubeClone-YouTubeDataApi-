50c10885ad195534acd60c67ed1c3fb017559dbb
Aung ThuKha20 <athukha20@gmail.com>
2024-02-23 22:45:04 +0630

    update....

--- src/Components/PlayVideo.jsx
+++ src/Components/PlayVideo.jsx
@@ -7,8 +7,13 @@ import { IoMdShareAlt } from "react-icons/io";
 import { HiSave } from "react-icons/hi";
 import { BsClockHistory, BsThreeDots } from "react-icons/bs";
 const PlayVideo = ({ id, video }) => {
-  // console.log(video);
+  console.log(video);
+  const [more, setMore] = useState(false);
+  const moreHandler = () => {
+    setMore(!more);
+  };
   const formattedDate = moment(video.snippet.publishedAt).fromNow();
+  const view = viewConverter(video.statistics.viewCount);
   const [channel, setChannel] = useState(null);
   const subscriber = viewConverter(channel?.statistics.subscriberCount);
   useEffect(() => {
@@ -19,15 +24,16 @@ const PlayVideo = ({ id, video }) => {
       `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${video.snippet.channelId}&key=${api_key}`
     );
     const channelData = await api.json();
+
     console.log(channelData.items[0]);
     setChannel(channelData.items[0]);
   };
 
   return (
     <div className=" flex md:justify-end justify-center  md:w-full w-screen   ">
-      <div className="  flex-col     md:me-4 me-0 ">
+      <div className="  flex-col  md:w-[60vw] w-screen    md:me-4 me-0 ">
         <iframe
-          className=" mt-1 flex h-[50vh]  rounded-md md:h-[70vh] justify-center w-screen md:w-[60vw]"
+          className=" mt-1 flex h-[40vh]  rounded-md md:h-[70vh] justify-center w-screen md:w-[60vw]"
           src={`https://www.youtube.com/embed/${id}?autoplay=1`}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         ></iframe>
@@ -35,14 +41,6 @@ const PlayVideo = ({ id, video }) => {
           <p className=" font-semibold text-[18px] ">{video.snippet.title}</p>
           <div className="   ">
             <div className=" flex flex-wrap items-center mt-2 justify-between">
-              {/* <p className=" text-[8px] flex items-center  text-gray-400">
-                {viewConverter(video.statistics.viewCount)} views{" "}
-                <span>
-                  <LuDot size={20} />
-                </span>
-                {formattedDate}
-              </p> */}
-
               {channel && (
                 <div className=" flex md:justify-start justify-between md:w-auto w-full items-center gap-2">
                   <div className=" flex items-center gap-4">
@@ -52,22 +50,22 @@ const PlayVideo = ({ id, video }) => {
                       alt=""
                     />
                     <div>
-                      <p className=" md:text-[14px] text-[10px] font-medium">
+                      <p className=" md:text-[16px] text-[10px] font-medium">
                         {channel.snippet.title}
                       </p>
-                      <p className=" text-[2px] text-gray-400">
+                      <p className=" text-[14px] text-gray-400">
                         {subscriber}
                         <span className="   ms-1">Subscribers</span>
                       </p>
                     </div>
                   </div>
-                  <button className=" text-[1px] px-3 py-[1px] ms-2 font-semibold hover:bg-gray-300 bg-white text-black rounded-full">
+                  <button className=" text-[16px] px-3 py-[1px] me-4 md:me-0 font-semibold hover:bg-gray-300 bg-white text-black rounded-full">
                     Subscribe
                   </button>
                 </div>
               )}
 
-              <div className=" flex  items-center justify-start w-full md:w-auto md:mt-0 mt-3 text-[4px] md:gap-4 gap-6">
+              <div className=" flex  items-center justify-start w-full md:w-auto md:mt-0 mt-3 text-[16px] md:gap-4 gap-6">
                 <div className=" flex  ">
                   <button className=" border-e  border-gray-600 flex items-center rounded-s-full hover:bg-gray-800 bg-gray-900 px-2 py-[1px]">
                     <AiFillLike size={15} />{" "}
@@ -88,6 +86,34 @@ const PlayVideo = ({ id, video }) => {
                   <HiSave className=" me-1" size={15} />
                   Save
                 </button>
+              </div>
+            </div>
+            <div className=" bg-gray-900 mt-4 md:p-4 p-2  ">
+              <div className=" flex items-center md:gap-4 gap-3 text-[14px]">
+                <p className="  font-semibold">
+                  {view} <span>views</span>
+                </p>
+                <p className=" font-semibold">{formattedDate}</p>
+                <div className="flex text-gray-400 font-semibold items-center gap-2">
+                  {video.snippet.tags?.slice(0, 3).map((tag, index) => (
+                    <p key={index}>#{tag}</p>
+                  ))}
+                </div>
+              </div>
+              <div className=" w-full">
+                <p className="mt-2 text-[14px] font-medium">
+                  {more
+                    ? video.snippet.description
+                    : `${video.snippet.description.substring(0, 200)}`}
+                  {video.snippet.description.length > 200 && (
+                    <button
+                      onClick={moreHandler}
+                      className="text-blue-500 ms-2 hover:underline focus:outline-none"
+                    >
+                      {more ? "Show less" : "...more"}
+                    </button>
+                  )}
+                </p>
               </div>
             </div>
           </div>
